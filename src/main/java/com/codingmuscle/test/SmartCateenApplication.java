d88c13491647920f3d6b34aa7dece8f45fd1d14b
codingmuscle <yangwanlipt@gmail.com>
2024-05-06 22:23:02 +0800

    Initial commit

---
+++ src/main/java/com/codingmuscle/test/SmartCateenApplication.java
@@ -0,0 +1,13 @@
+package com.codingmuscle.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCateenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCateenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codingmuscle/test/SmartCateenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingmuscle.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCateenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
